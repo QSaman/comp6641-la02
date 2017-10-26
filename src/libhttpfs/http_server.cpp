703c3390bbec97a4b27764aac03c955fde7348fa
Saman Saadi <QSaman@users.noreply.github.com>
2017-10-26 19:18:15 -0400

    Fix reading socket data in the server.

--- src/libhttpfs/http_server.cpp
+++ src/libhttpfs/http_server.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <thread>
 #include <fstream>
+#include <algorithm>
 
 std::string root_dir_path = "./resources/https_root";
 static int level = 0;
@@ -126,59 +127,84 @@ std::string prepareReplyMessage(const HttpMessage& client_msg) noexcept
     }
 }
 
-void handleClientHttpRequest(asio::ip::tcp::socket active_socket)
+void readAndWrite(asio::ip::tcp::socket& active_socket)
 {
-    asio::error_code ec;
     std::string http_header;
-
-    while (true)
-    {
-        asio::streambuf header_sb(4);
-        auto n = asio::read(active_socket, header_sb, ec);
-        if (ec && ec != asio::error::eof)
-        {
-            std::cerr << "Error in reading data (" << ec.value() << "): " << ec.message() << std::endl;
-            exit(ec.value());
-        }
-        auto bufs = header_sb.data();
-        auto tmp = std::string(asio::buffers_begin(bufs), asio::buffers_begin(bufs) + n);
-        if (tmp == "\r\n\r\n" || tmp.empty())
-            break;
-        http_header += tmp;
-    }
+    asio::streambuf buffer;
+    auto n = asio::read_until(active_socket, buffer, "\r\n\r\n");
+    auto iter = asio::buffers_begin(buffer.data());
+    http_header = std::string(iter, iter + n);
     std::cout << "header:" << std::endl << http_header << std::endl;
     HttpMessage http_message = HttpClient::extractMessage(http_header, false);
     std::string length_str = http_message.http_header["Content-Length"];
     std::istringstream iss(length_str);
     unsigned int length;
-    iss >> length;
-    asio::streambuf body_sb(length);
-    auto n = asio::read(active_socket, body_sb, ec);
-    if ((ec && ec != asio::error::eof) || n != length)
+    iss >> length;    
+    //We need to use the same buffer after consuming the header data:
+    //It is important to remember that there may be additional data after the delimiter.
+    //For more information: http://think-async.com/Asio/asio-1.11.0/doc/asio/overview/core/line_based.html
+    buffer.consume(n);
+    auto partial_len = std::min(static_cast<unsigned int>(buffer.size()), length);
+    std::string body;
+    if (partial_len > 0)
     {
-        std::cerr << "Error in reading data (" << ec.value() << "): " << ec.message() << std::endl;
-        exit(ec.value());
+        iter = asio::buffers_begin(buffer.data());
+        body = std::string(iter, iter + partial_len);
+        buffer.consume(partial_len);
     }
-    auto bufs = body_sb.data();
-    auto body = std::string(asio::buffers_begin(bufs), asio::buffers_begin(bufs) + n);
+    auto num_bytes = length - body.length();
+    if (num_bytes > 0)
+    {
+        n = asio::read(active_socket, buffer, asio::transfer_exactly(num_bytes));
+        iter = asio::buffers_begin(buffer.data());
+        body += std::string(iter, iter + num_bytes);
+        buffer.consume(num_bytes);
+    }
     http_message.body = body;
     std::cout << "body: " << std::endl << body << std::endl;
-    asio::write(active_socket, asio::buffer(prepareReplyMessage(http_message)), ec);
-    if (ec)
-        std::cerr << "Error in writing data (" << ec.value() << "): " << ec.message() << std::endl;
+    asio::write(active_socket, asio::buffer(prepareReplyMessage(http_message)));
+    if (buffer.size() > 0)
+        std::cerr << "Client sends more than one request without getting the replies" << std::endl;
 }
 
-[[noreturn]] void runUdpServer(unsigned short port)
+void handleClientHttpRequest(asio::ip::tcp::socket active_socket) noexcept
+{
+    try
+    {
+        readAndWrite(active_socket);
+    }
+    catch(const std::system_error& ex)
+    {
+        std::cerr << ex.what() << std::endl;
+    }
+    catch(...)
+    {
+        std::cerr << "Unknown exception for active socket " << std::endl;
+    }
+}
+
+void runUdpServer(unsigned short port)
 {
     using asio::ip::tcp;
     asio::io_service io_service;
     std::cout << "Listening on port " << port << std::endl;
-    tcp::acceptor passive_socket(io_service, tcp::endpoint(tcp::v4(), port));
-    while (true)
+    try
     {
-        tcp::socket active_socket(io_service);
-        passive_socket.accept(active_socket);
-        std::thread(handleClientHttpRequest, std::move(active_socket)).detach();
+        tcp::acceptor passive_socket(io_service, tcp::endpoint(tcp::v4(), port));
+        while (true)
+        {
+            tcp::socket active_socket(io_service);
+            passive_socket.accept(active_socket);
+            std::thread(handleClientHttpRequest, std::move(active_socket)).detach();
+        }
+    }
+    catch(const std::system_error& ex)
+    {
+        std::cerr << ex.what() << std::endl;
+    }
+    catch(...)
+    {
+        std::cerr << "Unknown exception for passive socket" << std::endl;
     }
 }
 
--- src/libhttpfs/http_server.h
+++ src/libhttpfs/http_server.h
@@ -4,5 +4,5 @@
 
 extern std::string root_dir_path;
 
-[[noreturn]] void runUdpServer(unsigned short port);
+void runUdpServer(unsigned short port);
 std::string constructServerMessage(const std::string partial_header, const std::string body);
